0fb626924677ba2183494c56f5e87383be9b34a3
KiyaG <hamilee.kiya@gmail.com>
2017-07-02 10:36:24 +0100

    implemented the function for the find-alive-people function

--- 01-exercises/find-alive-people.test.js
+++ 01-exercises/find-alive-people.test.js
@@ -1,55 +1,61 @@
 function findAlive(writers) {
-  // ToDo: Implement this function to make all the tests below pass
-  // DO NOT Change the tests (you can add more tests though)
+    // ToDo: Implement this function to make all the tests below pass
+    // DO NOT Change the tests (you can add more tests though)
+    return writers.filter(function(value) {
+        if (value.alive === true) {
+            return value;
+        }
+    });
+
 }
 
 
 test('function should retrieve only alive writers', function() {
-  var persons = [
-    {
-      firstName: "Virginia",
-      lastName: "Woolf",
-      occupation: "writer",
-      age: 59,
-      alive: false
-    },
-    {
-      firstName: "Zadie",
-      lastName: "Smith",
-      occupation: "writer",
-      age: 41,
-      alive: true
-    },
-    {
-      firstName: "Jane",
-      lastName: "Austen",
-      occupation: "writer",
-      age: 41,
-      alive: false
-    },
-    {
-      firstName: "bell",
-      lastName: "hooks",
-      occupation: "writer",
-      age: 64,
-      alive: true
-    },
-  ];
+    var persons = [{
+            firstName: "Virginia",
+            lastName: "Woolf",
+            occupation: "writer",
+            age: 59,
+            alive: false
+        },
+        {
+            firstName: "Zadie",
+            lastName: "Smith",
+            occupation: "writer",
+            age: 41,
+            alive: true
+        },
+        {
+            firstName: "Jane",
+            lastName: "Austen",
+            occupation: "writer",
+            age: 41,
+            alive: false
+        },
+        {
+            firstName: "bell",
+            lastName: "hooks",
+            occupation: "writer",
+            age: 64,
+            alive: true
+        },
+    ];
 
-  var result = findAlive(persons);
+    var result = findAlive(persons);
 
-  expect(result).toEqual([{
-      firstName: "Zadie",
-      lastName: "Smith",
-      occupation: "writer",
-      age: 41,
-      alive: true
-    },
-    {
-      firstName: "bell",
-      lastName: "hooks",
-      occupation: "writer",
-      age: 64,
-      alive: true
-    }])
+    expect(result).toEqual([{
+            firstName: "Zadie",
+            lastName: "Smith",
+            occupation: "writer",
+            age: 41,
+            alive: true
+        },
+        {
+            firstName: "bell",
+            lastName: "hooks",
+            occupation: "writer",
+            age: 64,
+            alive: true
+        }
+    ])
 });
