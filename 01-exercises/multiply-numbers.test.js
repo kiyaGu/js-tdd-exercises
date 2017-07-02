ab486736ea78c20ce3ccf2cf54fca2446b6a532e
KiyaG <hamilee.kiya@gmail.com>
2017-07-02 10:41:45 +0100

    implemented the function multiply-numbers and wrote the test cases for it

--- 01-exercises/multiply-numbers.test.js
+++ 01-exercises/multiply-numbers.test.js
@@ -11,3 +11,33 @@
         3. Refactor if needed
         4. REPEAT
  */
+function multiplies() {
+    // this line converts whatever is passed to the function into an array
+    // read more here - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+    var args = Array.prototype.slice.call(arguments);
+    var result = 1;
+    for (var i = 0; i < args.length; i++) {
+        result *= args[i];
+    }
+    return result;
+}
+
+test('multiplies should multiply two numbers', function() {
+    var result = multiplies(1, 0);
+    expect(result).toEqual(0);
+});
+
+test('multiplies should multiply two numbers', function() {
+    var result = multiplies(0, 0);
+    expect(result).toEqual(0);
+});
+
+test('multiplies should multiply two numbers', function() {
+    var result = multiplies(1, -2);
+    expect(result).toEqual(-2);
+});
+
+test('multiplies should multiply two numbers', function() {
+    var result = multiplies(4, 5);
+    expect(result).toEqual(20);
+});
